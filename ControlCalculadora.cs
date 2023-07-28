63e7a627a8c1ba3d7b0abdec026395259266246e
GABRIEL DE OLIVEIRA CARNAVAL <gabriel.ocarnaval@senacsp.edu.br>
2023-07-28 15:42:32 -0300

    primeiro commit

---
+++ ControlCalculadora.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class ControlCalculadora
+    {
+        //Criando um objeto calculadora na memória
+        ModelCalculadora calculadora;
+
+        public ControlCalculadora()
+        {
+            this.calculadora = new ModelCalculadora();
+        }//fim do construtor
+
+        public void Coletar()
+        {
+            Console.WriteLine("Informe um número: ");
+            this.calculadora.GetSetNum1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe outro número: ");
+            this.calculadora.GetSetNum2= Convert.ToDouble(Console.ReadLine());
+        }//fim do coletar
+
+        public void ColetarBhaskara()
+        {
+            Console.WriteLine("Informe o 1 número: ");
+            this.calculadora.GetSetNum1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o 2 número: ");
+            this.calculadora.GetSetNum2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o 3 número: ");
+            this.calculadora.GetSetNum3 = Convert.ToDouble(Console.ReadLine());
+        }//fim do coletarBhaskara
+
+        public void ColetarRaiz()
+        {
+            Console.WriteLine("informe um número: ");
+            this.calculadora.GetSetNum1 = Convert.ToDouble(Console.ReadLine()) ;
+        }// fim do coletarRaiz
+
+        public int Menu()
+        {
+            Console.WriteLine("---------------Menu---------------"       +
+                                "\n0. Sair"                              +
+                                "\n1. Somar"                             +
+                                "\n2. Subtrair"                          +
+                                "\n3. Dividir"                           +
+                                "\n4. Multiplicar"                       +
+                                "\n5. Potência"                          +
+                                "\n6. Raiz"                              +
+                                "\n7. Tabuada"                           +     
+                                "\n8. Bhaskara"                          +
+                                "\n9. Binário -> Decimal"                +
+                                "\n10. Decimal -> Binário"               +
+                                "\n11. Hexadecimal -> Decimal"           +
+                                "\n\n--DAQUI PRA BAIXO SÃO EXERCICIOS--" +
+                                "\n\n12. Dobro e Triplo do seu número"   +
+                                "\n13. Salário + 30%"                    +
+                                "\n14. ImPar, positivos e negativos"     +
+                                "\n15. Soma todos os numeros de 1 a 100" +
+                                "\n16. Tabuada de 1 à N"                 +
+                                "\n17. Contador"                         +
+                                "\n18. Impares de 100 a 200"             +
+                                "\n19. "+         
+                                "\n\n Escolha uma das opções acima: ");
+            int opcao = Convert.ToInt32(Console.ReadLine());
+            return opcao;
+        }//Fim do método Menu
+         public void Operacao()
+         {
+            int opcao = -1;
+            do
+            {
+                opcao = Menu();
+                Console.Clear();
+
+                switch (opcao)
+                {
+                    case 0:
+                        Console.WriteLine("Obrigado!");
+                        break;
+                    case 1:
+                        Coletar();
+                        Console.WriteLine("Soma: " + this.calculadora.Somar());
+                        break;
+                    case 2:
+                        Coletar();
+                        Console.WriteLine("Subtração: " + this.calculadora.Subtrair());
+                        break;
+                    case 3:
+                        Coletar();
+                        if (this.calculadora.Dividir() == -1)
+                        {
+                            Console.WriteLine("Impossível dividir!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Divisão: " + this.calculadora.Dividir());
+                        }
+                        break;
+                    case 4:
+                        Coletar();
+                        Console.WriteLine("Multiplicar: " + this.calculadora.Multiplicar());
+                        break;
+                    case 5:
+                        Coletar();
+                        Console.WriteLine("Potência: " + this.calculadora.Potencia());
+                        break;
+                    case 6:
+                        ColetarRaiz();
+                        Console.WriteLine("Raiz: " + this.calculadora.Raiz());
+                        break;
+                    case 7:
+                        Console.WriteLine("Informe um número: ");
+                        this.calculadora.GetSetNum1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(this.calculadora.TabuadaNum1());
+                        break;
+
+                    case 8:
+                        ColetarBhaskara();
+                        Console.WriteLine("Conversão: " + this.calculadora.Bhaskara());
+                        break;
+
+                    case 9:
+                        Console.WriteLine("Informe o numero binário:");
+                        string valor = Console.ReadLine();
+                        int resultado = Convert.ToInt32(ModelCalculadora.BinarioDecimal(valor));
+                        Console.WriteLine($" O numero {valor} é igual a {resultado} ");
+                        break;
+
+                    case 10:
+                        Console.WriteLine("Informe o numero decimal a converter");
+                        string valor1 = Console.ReadLine();
+                        string resultado1 = ModelCalculadora.DecimalBinario(valor1);
+                        Console.WriteLine($" O numero {valor1} é igual a {resultado1} ");
+                        break;
+
+                    case 11:
+                        Console.WriteLine("Digite um número hexadecimal:");
+                        string hexInput = Console.ReadLine();
+                        ModelCalculadora model3 = new ModelCalculadora();
+                        try
+                        {
+                            int decimalNumber1 = model3.HexadecimalDecimal(hexInput);
+                            Console.WriteLine($"O número decimal equivalente é: {decimalNumber1}");
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+
+
+                        break;
+
+                    case 12:
+                        ColetarRaiz();
+                        Console.WriteLine(this.calculadora.Dobrotriplo());
+                        break;
+
+                    case 13:
+                        ColetarRaiz();
+                        Console.WriteLine(this.calculadora.Salario30());
+                        break;
+
+                    case 14:
+                        ColetarRaiz();
+                        Console.WriteLine(this.calculadora.Imparpositivonegativo());
+                        break;
+
+                    case 15:
+                        ModelCalculadora modelCalculadora = new ModelCalculadora();
+                        int inicio = 1;
+                        int fim = 100;
+                        int soma = modelCalculadora.Soma1a100(inicio, fim);
+                        Console.WriteLine(soma);
+                       break;
+
+                    case 16:
+                        ColetarRaiz();
+                        Console.WriteLine(this.calculadora.Tabuada1N());
+                        break;
+
+                    case 17:
+                        Console.WriteLine("Informe o 1° numero: ");
+                             int n1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Informe o 2° número:");
+                             int n2 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(this.calculadora.Contador(n1,n2));
+                        break;
+
+                    case 18:
+                        Console.WriteLine(this.calculadora.Impares100200());
+                        break;
+        
+                    default:
+                        Console.WriteLine("Opção escolhida não é valida");
+                        break;
+                   
+
+                }//fim do switch
+            } while (opcao != 0);//fim do Do...while
+        }// fim do operacao
+    }//fim da classe
+}//fim do projeto
---
+++ ModelCalculadora.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class ModelCalculadora
+    {
+        //declarando variaveis
+        private double num1;
+        private double num2;
+        private double num3;
+
+        public ModelCalculadora()
+        {
+            GetSetNum1 = 0;
+            GetSetNum2 = 0;
+            GetSetNum2 = 0;
+        }//fim do construtor
+
+        //Métodos gets e sets
+        public double GetSetNum1
+        {
+            get { return this.num1; }
+            set { this.num1 = value; }
+        }//fim do GetSetNum1
+
+        public double GetSetNum2
+        {
+            get { return this.num2; }
+            set { this.num2 = value; }
+        }//fim do GetSetNum2
+
+        public double GetSetNum3
+        {
+            get { return this.num3; }
+            set { this.num3 = value; }
+        }
+
+        //Métodos
+        public double Somar()
+        {
+            return GetSetNum1 + GetSetNum2;
+        }//fim do método Somar
+
+        public double Subtrair()
+        {
+            return GetSetNum1 - GetSetNum2;
+        }//fim do método Subtrair
+
+        public double Dividir()
+        {
+            if (GetSetNum2 <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return GetSetNum1 / GetSetNum2;
+            }
+        }//fim do método Dividir
+
+        public double Multiplicar()
+        {
+            return GetSetNum1 * GetSetNum2;
+        }//fim do método Multiplicar
+
+        public double Potencia()
+        {
+            return Math.Pow(GetSetNum1, GetSetNum2);
+        }//fim da potencia
+
+        public string Raiz()
+        {
+            string msg = "\nRaíz do primeiro número: " + Math.Sqrt(GetSetNum1);
+            return msg;
+        }//fim do método raiz
+
+        public string TabuadaNum1()
+        {
+            string resultado = "";
+
+            for (int i = 0; i <= 10; i++)
+            {
+                resultado += "\n" + GetSetNum1 + " * " + i + " = " + (GetSetNum1 * i);
+            }//fim do método for
+
+            return resultado;
+        }//fim do método 
+
+        public string TabuadaNum2()
+        {
+            string resultado = "";
+
+            for (int i = 0; i <= 10; i++)
+            {
+                resultado += "\n" + GetSetNum2 + " * " + i + " = " + (GetSetNum2 * i);
+            }//fim do método for
+
+            return resultado;
+        }//fim do método 
+
+        public double Bhaskara()
+        {
+            return (GetSetNum2 * GetSetNum2) - (4) * (GetSetNum1 * GetSetNum3);
+        }//Fim do método Delta
+
+
+
+        //Método X1
+        public double X1()
+        {
+            return (GetSetNum2 + Math.Sqrt(Bhaskara())) / (2 * GetSetNum1);
+        }//Fim do método X1
+
+
+
+        //Método X1
+        public double X2()
+        {
+            return (-GetSetNum2 - Math.Sqrt(Bhaskara())) / (2 * GetSetNum1);
+        }//Fim do método X2
+
+        public static string ReverteString(string str)
+        {
+            return new string(str.Reverse().ToArray());
+        }//fim do reverte string
+
+        public static int BinarioDecimal(string numeroBinario)
+        {
+            int expoente = 0;
+            int numero;
+            int soma = 0;
+            string numeroInvertido = ReverteString(numeroBinario);
+            for (int i = 0; i < numeroInvertido.Length; i++)
+            {
+                numero = Convert.ToInt32(numeroInvertido.Substring(i, 1));
+                soma += numero * (int)Math.Pow(2, expoente);
+                expoente++;
+            }//fim do for
+            return soma;
+        }//fim do binario para decimal
+
+        public static string DecimalBinario(string numeroDecimal)
+        {
+            string valor1 = "";
+            int dividendo = Convert.ToInt32(numeroDecimal);
+            if (dividendo == 0 || dividendo == 1)
+            {
+                return Convert.ToString(dividendo);
+            }
+            else
+            {
+                while (dividendo > 0)
+                {
+                    valor1 += Convert.ToString(dividendo % 2);
+                    dividendo = dividendo / 2;
+                }//fim do while
+                return ReverteString(valor1);
+            }//fim do if/else
+        }//fim do decimal para binario
+
+        public int HexadecimalDecimal(string hexNumber)
+        {
+            int decimalNumber1;
+            try
+            {
+                decimalNumber1 = Convert.ToInt32(hexNumber, 16);
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException("O número hexadecimal inserido é inválido.");
+            }
+            return decimalNumber1;
+
+
+        }//fim da classe
+
+        public string Dobrotriplo()
+        {
+            return "O dobro e o triplo do seu número são: " + (GetSetNum1 * 2) + " e " + (GetSetNum1 * 3);
+        }
+
+        public string Salario30()
+        {
+            return "O seu salário aumentado em 30% é de: " + (GetSetNum1 * 30 / 100 + GetSetNum1);
+        }
+
+        public string Imparpositivonegativo()
+        {
+            string resultado = "";
+            string ResultadoParImpar = "";
+
+            if (GetSetNum1 > 0)
+                resultado += "Este número é um positivo";
+            else
+                resultado += "Este número é um negativo";
+
+            if (GetSetNum1 % 2 == 0)
+                ResultadoParImpar += " par!";
+            else
+                ResultadoParImpar += " impar!";
+
+            return resultado + ResultadoParImpar;
+        }
+
+
+        public int Soma1a100(int inicio, int fim)
+        {
+            int soma = 0;
+            for (int i = inicio; i <= fim; i++)
+            {
+                soma += i;
+            }
+            return soma;
+        }// fim do Exercicio 4
+
+        public string Tabuada1N()
+        {
+            string resultado = "";
+
+            for (int i = 1; i <= GetSetNum1; i++)
+            {
+                resultado += "\n" + 1 + " * " + i + " = " + (1 * i);
+            }//fim do método for
+
+            return resultado;
+        }// fim tabuada de 1 para N
+
+        public string Contador(int n1, int n2)
+        {
+            string resultado = "";
+
+            for (int i = n1; i <= n2; i++)
+            {
+                resultado += "\n" + (n1 + (1 * i - n1));
+            }//fim do método for
+
+            return resultado;
+        }// fim contador
+
+        public string Impares100200()
+        {
+            string ResultadoImpar = "";
+
+            for (int i = 100; i <= 200; i++)
+            {
+                if (i % 2 == 1)
+                    ResultadoImpar += i;
+                else;
+                    ResultadoImpar+= "\n";
+                    
+            }
+
+            return ResultadoImpar;
+        }//fim Impares100200
+
+        public string Deznumerosesoma()
+        {
+            string resultado = "";
+            for (int i = 1; i <= 10; i++)
+            {
+                
+            }
+
+            return resultado;
+        }
+    }// fim ModelCalculadora
+}//fim do projeto
